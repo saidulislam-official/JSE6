15ebd8c517ef0ec994fe2bdaa92a2056ef3efc41
saidulislam-official <saidulislam.own@gmail.com>
2025-07-17 10:12:05 +0600

    added loop

---
+++ keys.js
@@ -0,0 +1,11 @@
+const glass = {
+    name : 'glass',
+    color : 'golden',
+    price : 65,
+    isCleaned : true,
+};
+console.log(glass);
+const keys = Object.keys(glass);
+console.log(keys);
+const values = Object.values(glass);
+console.log(values);
